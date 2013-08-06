015f82b639f496076499b049716083c321cd5366
John Murowaniecki <jmurowaniecki@gmail.com>
2013-08-06 07:13:57 -0300

    teste encapsulado

--- minas-encantadas.py
+++ minas-encantadas.py
@@ -22,7 +22,7 @@ class MinasEncantadas:
 		for x in self.terreno:
 			mstr = ""
 			for y in x:
-				mstr += "%d\t" % y
+				mstr += "%d\t" % y 
 			print mstr
 
 
@@ -53,22 +53,26 @@ class MinasEncantadas:
 		return soma
 
 
-mina = MinasEncantadas(300, 300, -50, 50)
-mina.exibe()
+class Teste:
+	def __init__(self, x, y, vmin, vmax):
+		mina = MinasEncantadas(x, y, vmin, vmax)
+		mina.exibe()
 
-for px in xrange(2, mina.dimensoes[0]):
-	for py in xrange(2, mina.dimensoes[1]):
-		mina.processa([px, py])
+		for px in xrange(2, mina.dimensoes[0] / 2):
+			for py in xrange(2, mina.dimensoes[1] / 2):
+				mina.processa([px, py])
 
-ideal = Posicao(0, 0, [0, 0], 0)
+		ideal = Posicao(0, 0, [0, 0], 0)
 
-for melhor in mina.busca:
-	if type(melhor) == int:
-		print "melhor is integer %d" % melhor
-	if type(ideal.valor) != int:
-		print "ideal valor is not integer %d" % melhor.valor
-	if melhor.valor > ideal.valor:
-		ideal = melhor
-	print "O melhor valor para matrizes de %dx%d foi %d nas coordenadas x, y (%d, %d)." % (melhor.proporcao[0], melhor.proporcao[1], melhor.valor, melhor.x, melhor.y)
+		for melhor in mina.busca:
+			if type(melhor) == int:
+				print "melhor is integer %d" % melhor
+			if type(ideal.valor) != int:
+				print "ideal valor is not integer %d" % melhor.valor
+			if melhor.valor > ideal.valor:
+				ideal = melhor
+			print "O melhor valor para matrizes de %dx%d foi %d nas coordenadas x, y (%d, %d)." % (melhor.proporcao[0], melhor.proporcao[1], melhor.valor, melhor.x, melhor.y)
 
-print "O valor ideal para matrizes de %dx%d foi %d nas coordenadas x, y (%d, %d)." % (ideal.proporcao[0], ideal.proporcao[1], ideal.valor, ideal.x, ideal.y)
+		print "O valor ideal para matrizes de %dx%d foi %d nas coordenadas x, y (%d, %d)." % (ideal.proporcao[0], ideal.proporcao[1], ideal.valor, ideal.x, ideal.y)
+
+Teste(30, 30, -50, 50)
